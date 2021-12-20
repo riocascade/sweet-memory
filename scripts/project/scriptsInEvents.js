b03b9adfa8a6579540dad2bc50258736682b130d
riocascade <riocascade@gmail.com>
2021-12-20 22:46:14 +0700

    sweet memory

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -5,24 +5,22 @@ const scriptsInEvents = {
 
 		async EventSheet1_Event34_Act2(runtime, localVars)
 		{
-			globalThis.parent.postMessage({message: 'level:'+runtime.globalVars["level"]}, "*");
+			window.parent.postMessage({message: 'level:'+runtime.globalVars["level"]}, "*");
 		},
 
 		async EventSheet1_Event35_Act3(runtime, localVars)
 		{
-			globalThis.parent.postMessage({message: 'finish'}, "*");
+			window.parent.postMessage({message: 'finish'}, "*");
 		},
 
 		async EventSheet1_Event36_Act2(runtime, localVars)
 		{
-			globalThis.parent.postMessage({message: 'finish'}, "*");
+			window.parent.postMessage({message: 'finish'}, "*");
 		},
 
 		async E_first_Event1_Act1(runtime, localVars)
 		{
-			window.parent.postMessage({message: 'finish'}, "*");
-			
-			globalThis.addEventListener ("message", function (event) {
+			window.addEventListener ("message", function (event) {
 			if (event.data == "start_game") {
 			runtime.globalVars["isstart"] = 1;
 			}
